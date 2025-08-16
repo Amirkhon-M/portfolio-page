8d436e31a3b6e56b9d43e016083f117a705f53c4
Amirkhon <amirkonmahmadaminov@gmail.com>
2025-08-16 19:00:45 +0500

    dark-theme

--- index.js
+++ index.js
@@ -18,11 +18,21 @@ document.addEventListener('DOMContentLoaded', () => {
     });
 
     // Add scroll effect to header for styling
+    let lastScrollY = window.scrollY;
     window.addEventListener('scroll', () => {
         if (window.scrollY > 50) {
             mainHeader.classList.add('scrolled');
         } else {
             mainHeader.classList.remove('scrolled');
+        }
+
+        if (window.innerWidth <= 768) { // Only on mobile
+            if (lastScrollY < window.scrollY && window.scrollY > 100) {
+                mainHeader.classList.add("header-hidden");
+            } else {
+                mainHeader.classList.remove("header-hidden");
+            }
+            lastScrollY = window.scrollY;
         }
     });
 });
